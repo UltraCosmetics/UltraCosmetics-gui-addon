20785aede98823009b19bd2bea5272b1f4556d03
datatags <datatags@users.noreply.github.com>
2024-06-12 13:19:49 -0700

    Update resource pack to v1.1.0

--- src/main/java/me/datatags/ultracosmeticsguiaddon/ResourcePackListener.java
+++ src/main/java/me/datatags/ultracosmeticsguiaddon/ResourcePackListener.java
@@ -5,8 +5,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ResourcePackListener implements Listener {
-    public static final String PACK_URL = "https://github.com/dennishzg/uc-gui/releases/download/v1.0.0/UC-GUI-v1.0.0.zip";
-    public static final byte[] PACK_HASH = hexStringToByteArray("69e2f55fed03602a0f220d8524fa00771456343b");
+    public static final String PACK_URL = "https://github.com/dennishzg/uc-gui/releases/download/v1.1.0/UC-GUI-v1.1.0.zip";
+    public static final byte[] PACK_HASH = hexStringToByteArray("20D7DAFA7152EA5B75DA3113CB23D0C6D5C7EEE0");
     private final UltraCosmeticsGUIAddon plugin;
     public ResourcePackListener(UltraCosmeticsGUIAddon plugin) {
         this.plugin = plugin;
